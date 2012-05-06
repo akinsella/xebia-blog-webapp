0ad23c6bc376451f29e252f5e0d67c2ede0083eb
Alexis Kinsella <alexis.kinsella@gmail.com>
2012-05-06 02:04:05 +0200

    Add support for paging and post listing

--- javascript/app.js
+++ javascript/app.js
@@ -6,21 +6,24 @@ function onReadyState() {
     (function ($) {
 
         function initEventListeners() {
-            $("input").on('keyup', function() {
+            $('input').on('keyup', function() {
                 var start = new Date();
-                var search = $(this).val();
+                var search = $(this).val().toLowerCase();
                 if (!search) {
                     $('#master ul li').css('display', '');
                 }
                 else {
-                    var hideSelector = '#master ul li[data-name]:not(#master ul li[data-name*="' + $(this).val() + '"])';
-                    console.log('negativeSelector: ' + hideSelector);
-                    $(hideSelector).css('display', 'none');
-                    var showSelector = '#master ul li[data-name*="' + $(this).val() + '"]';
-                    $(showSelector).css('display', '');
+                    $('#master ul li[data-name]:not(#master ul li[data-name*="' + search + '"])').css('display', 'none');
+                    $('#master ul li[data-name*="' + search + '"]').css('display', '');
                 }
                 var end = new Date();
                 console.log("Bench: " + (end - start));
+            });
+
+            $('#master ul li').live('click', function() {
+                $('#master ul li.selected').removeClass('selected');
+                $(this).addClass('selected');
+                location.hash = '#/tag/' + $(this).attr('data-slug');
             });
 
         }
@@ -45,25 +48,45 @@ function initRouter() {
         $(parent).html('<div class="load-indicator-wrapper"><div class="load-indicator"></div></div>');
     }
 
+
     // OK
     var routes = {
         '/': function() {
             // Do Nothing
         },
-        '/tags': function() {
-            var page = 'tags';
-
+        '/tag': function() {
             showMasterLoadIndicator('#master-content');
 
             $.ajax({
                 url: 'http://blog.xebia.fr/wp-json-api/get_tag_index/?callback=?',
                 dataType: 'JSONP',
                 success: function(data) {
-                    var source   = $("#tag-item-template").html();
+                    var source = $("#tag-item-template").html();
                     var template = Handlebars.compile(source);
                     var html = template({ tags: data.tags });
 
                     $('#master-content').html(html);
+                },
+                error: function() {
+                    $('#master-content').html("");
+                }
+            });
+        },
+        '/tag/:slug?page=:page': function(tagSlug, page) {
+            showMasterLoadIndicator('#detail');
+
+            $.ajax({
+                url: 'http://blog.xebia.fr/wp-json-api//get_tag_posts/?exclude=description,parent,content,comments,attachments&tag_slug=' + tagSlug + '&page=' + page + '&count=8&callback=?',
+                dataType: 'JSONP',
+                success: function(data) {
+                    var source = $("#posts-tag-template").html();
+                    var template = Handlebars.compile(source);
+                    var html = template({ posts: data.posts });
+
+                    $('#detail').html(html);
+                },
+                error: function() {
+                    $('#detail').html("");
                 }
             });
         }
